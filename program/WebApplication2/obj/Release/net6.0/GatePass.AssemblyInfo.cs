d94eaabb78f4c23dc3436370b4f2bc5fe82add5d
Kavinda Kiridana <kavindakiridena@gmail.com>
2025-01-15 15:53:03 +0530

    updated the code

--- program/WebApplication2/obj/Release/net6.0/GatePass.AssemblyInfo.cs
+++ program/WebApplication2/obj/Release/net6.0/GatePass.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GatePass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b23a1aa480e5cd5a36864316ed4bcb790224f36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a3d33159d3aa1d2ee09437a343705f0f93d839c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GatePass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GatePass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
